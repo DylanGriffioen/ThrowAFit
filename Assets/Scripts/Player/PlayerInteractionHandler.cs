603aeb139036565ea02a4516a2ba7f090b59f48c
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2022-10-07 10:31:55 +0300

    Basic object pickup, drop and throw system.

--- Assets/Scripts/Player/PlayerInteractionHandler.cs
+++ Assets/Scripts/Player/PlayerInteractionHandler.cs
@@ -13,6 +13,7 @@ using UnityEngine.InputSystem;
  * Need to recalculate raycast position ( height )
  * Make pickedup object "follow" player
  * Throw object math
+ * Get values from throwable object
  */
 
 public class PlayerInteractionHandler : MonoBehaviour
@@ -20,11 +21,14 @@ public class PlayerInteractionHandler : MonoBehaviour
 
     [SerializeField] private LayerMask pickableLayer;
     [SerializeField] private float pickupRange = 2f;
+    [SerializeField] private float throwForce = 500f;
+    [SerializeField] private float throwUpwardForce = 300f;
     private PlayerControls playerControls;
 
     private RaycastHit hit;
 
     [SerializeField] private GameObject currentObject;
+    [SerializeField] private GameObject objectParent; // Pickable object parent
 
     // Start is called before the first frame update
     void Awake()
@@ -37,37 +41,33 @@ public class PlayerInteractionHandler : MonoBehaviour
         if (currentObject != null)
         {
             Debug.Log("Drop");
-            //drop
-            currentObject = null;
+            DropObject();
         }
         else
         {
             Debug.Log("Pickup");
             if (Physics.Raycast(transform.position, transform.forward, out hit, pickupRange, pickableLayer))
             {
-                currentObject = hit.collider.gameObject;
-
-                //pickup
+                PickupObject(hit.collider.gameObject);
             }
         }
-
     }
+
     private void OnThrow(InputValue value)
     {
         Debug.Log("Throw");
         if (currentObject == null)
             return;
 
-        //throw
-        currentObject.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * 20f;
-        currentObject = null;
+        Vector3 force = transform.forward * throwForce + transform.up * throwUpwardForce;
+        currentObject.GetComponent<Rigidbody>().AddForce(force);
+        DropObject();
     }
 
     private void OnPause(InputValue value)
     {
         Debug.Log("Pause");
     }
-
 
     /** (https://learn.unity.com/tutorial/taking-advantage-of-the-input-system-scripting-api?uv=2020.1&projectId=5fc93d81edbc2a137af402b7#5fcad3efedbc2a0020f781e1)
      * For the Input System commands to work, you will need to add both the OnEnable() and OnDisable() methods next.
@@ -80,5 +80,26 @@ public class PlayerInteractionHandler : MonoBehaviour
     private void OnDisable()
     {
         playerControls.Player.Disable();
+    }
+
+
+    private void PickupObject(GameObject go)
+    {
+        if (objectParent == null)
+            return;
+
+        currentObject = go;
+        currentObject.transform.position = Vector3.zero;
+        currentObject.transform.rotation = Quaternion.identity; // do we need this?
+        currentObject.transform.SetParent(objectParent.transform, false);
+    }
+
+    private void DropObject()
+    {
+        if (objectParent == null)
+            return;
+
+        currentObject.transform.parent = null;
+        currentObject = null;
     }
 }
