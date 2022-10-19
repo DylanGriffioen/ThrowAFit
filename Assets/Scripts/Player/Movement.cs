3100809c68181dbecff386308cf7c73a4abe3815
Lizene <olpertti@gmail.com>
2022-10-19 14:41:29 +0300

    Groundcheck
    Made a groundcheck object to the player prefab

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     InputActions input;
     Rigidbody rb;
+    GroundCheck groundCheck;
+
+    bool onGround = false;
 
     void Start()
     {
         input = new InputActions();
         rb = GetComponent<Rigidbody>();
+        groundCheck = transform.GetChild(4).GetComponent<GroundCheck>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        onGround = groundCheck.onGround;
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        
+    }
+
+    private void OnJump(InputValue value)
+    {
+        
+    }
+
+    private void OnCrouch(InputValue value)
+    {
+        Debug.Log("Crouch");
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
 
 
+    private void OnEnable()
+    {
+        input.Ingame.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Ingame.Disable();
+    }
 }
