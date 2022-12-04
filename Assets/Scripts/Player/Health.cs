f41cb907bcb563cf81b93ddada988e438d71dd60
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2022-12-04 18:53:14 +0200

    Health and life system.

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float maxHealth = 100f;
+    [SerializeField] float currentHealth;
+    [SerializeField] float respawnTime = 5f;
+    [SerializeField] bool alive;
+    [SerializeField] Life lifes;
+    [SerializeField] Vector3 respawnSpot;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentHealth = maxHealth;
+        alive = true;
+        respawnSpot = new Vector3(0f, 100f, 0f);
+    }
+
+    public void Heal(float amount)
+    {
+        if (alive)
+        {
+            currentHealth = (currentHealth + amount) > maxHealth ? maxHealth : (currentHealth + amount);
+        }
+    }
+
+    public void Damage(float amount)
+    {
+        if (alive)
+        {
+            currentHealth -= amount;
+
+            if (currentHealth <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    public void Kill()
+    {
+        currentHealth = 0;
+        alive = false;
+        OnDead();
+    }
+
+    private void OnDead()
+    {
+        if(lifes != null)
+        {
+            lifes.Lose(3);
+            //Animation?
+            PlayerComponents(false);
+            gameObject.transform.position = respawnSpot;
+            if (lifes.Alive)
+            {
+                StartCoroutine("Respawn");
+            }
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        currentHealth = maxHealth;
+        Respawn playerRespawner = gameObject.GetComponent<Respawn>();
+
+        if(playerRespawner != null)
+        {
+            PlayerComponents(true);
+            playerRespawner.RespawnPlayer();
+        }
+    }
+
+
+    private void PlayerComponents(bool activate)
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.detectCollisions = activate;
+        rb.useGravity = activate;
+        rb.velocity = Vector3.zero;
+        gameObject.GetComponent<Collider>().enabled = activate;
+        gameObject.GetComponent<Movement>().enabled = activate;
+    }
+}
---
+++ Assets/Scripts/Player/Life.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] int maxLifes = 3;
+    [SerializeField] int currentLifes;
+    public bool Alive { get; private set; }
+
+    void Awake()
+    {
+        currentLifes = maxLifes;
+        Alive = true;
+    }
+
+    public void Gain(int amount)
+    {
+        if (Alive)
+        {
+            currentLifes = (currentLifes + amount) > maxLifes ? maxLifes : (currentLifes + amount);
+        }
+    }
+
+    public void Lose(int amount)
+    {
+        if (Alive)
+        {
+            currentLifes -= amount;
+
+            if (currentLifes <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    public void Kill()
+    {
+        Alive = false;
+        OnDead();
+    }
+
+    private void OnDead()
+    {
+        Debug.Log($"Player {gameObject.name} died!");
+        // TODO: remove player..
+        //gameObject.SetActive(false); //Player input manager gives index out of bounds error..
+        //Destroy(gameObject); //Player input manager gives index out of bounds error..
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.detectCollisions = false;
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        gameObject.GetComponent<Collider>().enabled = false;
+        gameObject.GetComponent<Movement>().enabled = false;
+    }
+
+}
