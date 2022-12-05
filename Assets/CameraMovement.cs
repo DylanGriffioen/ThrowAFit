42072bd79bfc3a6f00446b583e4bade45fe94e01
KimiJokinen <kimi.jokinen@edu.turkuamk.fi>
2022-12-05 15:04:03 +0200

    Camera Movement Script
    Added camera that follows all of the players and zooms in slightly when players get closer to each other.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public List<Transform> targets;
+    public GameObject[] players;
+    public Vector3 offset = new Vector3(0, 8, -15);
+    private Vector3 velocity;
+    public float smoothTime = 0.5f;
+    public float minZoom = 70f;
+    public float maxZoom = 55f;
+    public float zoomLimiter = 30f;
+    private Camera cam;
+    public List<Transform> copylist;
+
+    Vector3 NewPosition = new Vector3(0, 20f, -10f);
+
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < players.Length; i++)
+        {
+            targets.Add(players[i].transform);
+        }
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+        Vector3 CenterPoint = GetCenterPoint();
+        if (CenterPoint.y > 10f && CenterPoint.y < 15f)
+        {
+            NewPosition = CenterPoint + offset;
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, NewPosition, ref velocity, smoothTime);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            if (targets[i].position.y < 10f && targets.Count > 1)
+            {
+                copylist.Add(targets[i]);
+                targets.Remove(targets[i]);
+            }
+        }
+        for (int i = 0; i < copylist.Count; i++)
+        {
+            if (copylist[i].transform.position.y > 20f)
+            {
+                targets.Add(copylist[i]);
+                copylist.Remove(copylist[i]);
+            }
+        }
+
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return Mathf.Max(bounds.size.x, bounds.size.z);
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
