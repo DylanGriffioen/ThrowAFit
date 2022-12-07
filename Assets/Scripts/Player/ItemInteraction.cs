2f1ae512250ff27a52e61ad04c197ff145d350ef
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2022-12-07 15:19:50 +0200

    Stealing item from player. (error fixed and added bool canSteal

--- Assets/Scripts/Player/ItemInteraction.cs
+++ Assets/Scripts/Player/ItemInteraction.cs
@@ -14,6 +14,7 @@ public class ItemInteraction : MonoBehaviour
     Animator animator;
     bool throwNextFrame;
     bool pickupDropEnabled = true;
+    bool _canSteal = true;
     void Awake()
     {
         itemSlot = transform.parent.GetChild(2);
@@ -45,7 +46,48 @@ public class ItemInteraction : MonoBehaviour
             heldObject = objectsInLootArea[closestIndex];
             objectsInLootArea.RemoveAt(closestIndex);
             var heldObjectTransform = heldObject.transform;
-            heldObjectTransform.parent = itemSlot;
+
+            GameObject parentObject = heldObjectTransform.parent == null ? null : heldObjectTransform.parent.gameObject;
+            if(parentObject == null || !parentObject.tag.Equals("Player"))
+            {
+                //From ground
+                heldObjectTransform.parent = itemSlot;
+                heldObjectTransform.localPosition = new Vector3(0f, heldObjectTransform.localScale.y / 2f, 0f);
+                heldObjectTransform.localRotation = Quaternion.identity;
+
+                //Store values and destroy the Rigidbody component
+                var rb = heldObject.GetComponent<Rigidbody>();
+                mass = rb.mass;
+                drag = rb.drag;
+                angularDrag = rb.angularDrag;
+                Destroy(rb);
+                movementScript.holdingItem = true;
+                movementScript.heldItemMass = mass;
+
+                //Switch carrying bool in animator
+                animator.SetBool("Carrying", true);
+            }
+            else
+            {
+                if (!_canSteal)
+                    return;
+                //Steal from player
+                GameObject stolenFromPlayer = parentObject.transform.parent.gameObject;
+                if(stolenFromPlayer != null)
+                {
+                    heldObjectTransform.parent = itemSlot;
+                    heldObjectTransform.localPosition = new Vector3(0f, heldObjectTransform.localScale.y / 2f, 0f);
+                    heldObjectTransform.localRotation = Quaternion.identity;
+                    stolenFromPlayer.GetComponentInChildren<ItemInteraction>().LoseItem();
+                    movementScript.holdingItem = true;
+                    movementScript.heldItemMass = mass;
+
+                    //Switch carrying bool in animator
+                    animator.SetBool("Carrying", true);
+                }
+            }
+
+            /*heldObjectTransform.parent = itemSlot;
             heldObjectTransform.localPosition = new Vector3(0f,heldObjectTransform.localScale.y/2f,0f);
             heldObjectTransform.localRotation = Quaternion.identity;
 
@@ -61,20 +103,31 @@ public class ItemInteraction : MonoBehaviour
             movementScript.heldItemMass = mass;
 
             //Switch carrying bool in animator
-            animator.SetBool("Carrying", true);
+            animator.SetBool("Carrying", true);*/
         }
 
         //Drop
         else if (itemSlot.childCount != 0)
         {
             movementScript.holdingItem = false;
-            heldObject.transform.parent = null;
+            heldObject.transform.parent = null; //TODO: If have time: Parent == ItemSpawner.item game object
 
             //Add Rigidbody and assign stored values
             var rb = heldObject.AddComponent<Rigidbody>();
             rb.mass = mass;
             rb.drag = drag;
             rb.angularDrag = angularDrag;
+
+            //Switch carrying bool in animator
+            animator.SetBool("Carrying", false);
+        }
+    }
+    public void LoseItem()
+    {
+        if (itemSlot.childCount == 0)
+        {
+            movementScript.holdingItem = false;
+            Destroy(heldObject);
 
             //Switch carrying bool in animator
             animator.SetBool("Carrying", false);
